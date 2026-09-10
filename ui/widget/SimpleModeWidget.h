2b373d19c6af7fb5b6f1c80d1a519f6a55f5aeb3
xenon <xenon@local>
2026-09-10 23:04:06 +0300

    ui: remove heroLegend (P↑ P↓ D↑ D↓) under hero button

--- ui/widget/SimpleModeWidget.h
+++ ui/widget/SimpleModeWidget.h
@@ -112,7 +112,6 @@ private:
     TrafficSparkline *heroChart = nullptr;
     QLabel *heroTitle = nullptr;
     QLabel *heroSub = nullptr; // status under the hero block
-    QLabel *heroLegend = nullptr;
 
 
 
